0bbba7b7ff4f62a0501e00ec9932d54919a0f3ba
Cubes786 <atechie2010@gmail.com>
2023-07-07 23:50:12 -0400

    final version

--- vscode/python3/lcialgo/2sum.py
+++ vscode/python3/lcialgo/2sum.py
@@ -1,1 +1,14 @@
-print('two sum')
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        remDict={}
+        for i,x in enumerate(nums):
+            if target-x in remDict:
+                return [remDict[target-x],i]
+            else:
+                remDict[x]=i
+        return [-1,-1]
+sol = Solution()
+print(sol.twoSum([2,7,11,15],9))
+print(sol.twoSum([3,3,11,15],6))
